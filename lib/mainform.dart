b442097ff830bdf27ca04158cdf2c9966ad70383
HasanBinZaker99 <hasanbinzaker99@gmail.com>
2023-11-19 20:50:53 -0330

    third commit

--- lib/mainform.dart
+++ lib/mainform.dart
@@ -9,9 +9,9 @@ class MainForm extends StatefulWidget {
 }
 
 class _MyFormState extends State<MainForm> {
-  var _productName;
   final _productController = TextEditingController();
-
+  final _productDesController = TextEditingController();
+  bool? _checkBox, _listTileCheckBox = false;
   @override
   void dispose() {
     // TODO: implement dispose
@@ -19,34 +19,54 @@ class _MyFormState extends State<MainForm> {
     super.dispose();
   }
 
-  void _updateText() {}
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Text("Form"),
-          actions: [],
         ),
         body: Container(
           padding: EdgeInsets.all(20.0),
           child: ListView(children: [
-            //  TextField(
-            //   decoration: InputDecoration(
-            //     hintText: "Product Name",
-            //     //icon: Icon(Icons.verified_user_outlined),
-            //     prefixIcon: Icon(Icons.add_alarm_sharp),
-            //   ),
-            // ),
-            TextFormField(
-              controller: _productController,
-              decoration: InputDecoration(
-                labelText: "Product Name",
-                prefixIcon: Icon(Icons.verified_outlined),
-                border: OutlineInputBorder(),
-              ),
+            const Text("Add product details in the form below"),
+            const SizedBox(height: 10.0),
+            MyTextField(
+                myController: _productController,
+                fieldName: "Product Name",
+                myIcon: Icons.propane_outlined,
+                prefixIconColor: Colors.deepPurple.shade300),
+            const SizedBox(height: 10.0),
+            MyTextField(
+                myController: _productDesController,
+                fieldName: "Product Description",
+                myIcon: Icons.propane_outlined,
+                prefixIconColor: Colors.deepPurple.shade300),
+            // CheckBox
+            Checkbox(
+                activeColor: Colors.yellow,
+                checkColor: Colors.black,
+                tristate: true,
+                value: _checkBox,
+                onChanged: (val) {
+                  setState(() {
+                    _checkBox = val;
+                  });
+                }),
+
+            // checkBox ListTile
+            CheckboxListTile(
+              title: Text("Top Product"),
+              value: _listTileCheckBox,
+              onChanged: (val) {
+                setState(() {
+                  _listTileCheckBox = val;
+                });
+              },
+              controlAffinity: ListTileControlAffinity.leading,
             ),
-            SizedBox(height: 20.0),
+
+            // Submit Button
+            const SizedBox(height: 10.0),
             myBtn(context),
           ]),
         ));
@@ -66,6 +86,38 @@ class _MyFormState extends State<MainForm> {
         "Submit Form".toUpperCase(),
         style: const TextStyle(fontWeight: FontWeight.bold),
       ),
+    );
+  }
+}
+
+class MyTextField extends StatelessWidget {
+  const MyTextField({
+    Key? key,
+    required this.fieldName,
+    required this.myController,
+    this.myIcon = Icons.verified_user_outlined,
+    this.prefixIconColor = Colors.blueAccent,
+  }) : super(key: key);
+  final TextEditingController myController;
+  final String fieldName;
+  final IconData myIcon;
+  final Color prefixIconColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: myController,
+      decoration: InputDecoration(
+          labelText: fieldName,
+          prefixIcon: Icon(
+            myIcon,
+            color: prefixIconColor,
+          ),
+          border: const OutlineInputBorder(),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.deepPurple.shade300),
+          ),
+          labelStyle: const TextStyle(color: Colors.deepPurple)),
     );
   }
 }
