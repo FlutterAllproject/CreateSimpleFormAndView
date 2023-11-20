0e34441d7f338899ed0167e07690b67665811a81
HasanBinZaker99 <hasanbinzaker99@gmail.com>
2023-11-19 23:34:54 -0330

    fourth commit

--- lib/mainform.dart
+++ lib/mainform.dart
@@ -1,6 +1,8 @@
 import 'package:create_simple_form/details.dart';
 import 'package:flutter/material.dart';
 
+enum ProductTypeEnum { Downloadable, Deliverable }
+
 class MainForm extends StatefulWidget {
   const MainForm({super.key});
 
@@ -9,25 +11,57 @@ class MainForm extends StatefulWidget {
 }
 
 class _MyFormState extends State<MainForm> {
+  _MyFormState() {
+    _selectedVal = _productSizesList[0];
+  }
+
   final _productController = TextEditingController();
   final _productDesController = TextEditingController();
   bool? _checkBox, _listTileCheckBox = false;
+  ProductTypeEnum? _productTypeEnum;
+
+  final _productSizesList = ["Small", "Medium", "Large", "XLarge"];
+  String? _selectedVal = "";
   @override
-  void dispose() {
-    // TODO: implement dispose
-    _productController.dispose();
-    super.dispose();
-  }
+  // void dispose() {
+  //   // TODO: implement dispose
+  //   _productController.dispose();
+  //   super.dispose();
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text("Form"),
+          backgroundColor: Colors.purple,
+          centerTitle: true,
+          title: const Text("Form Data",
+              style: TextStyle(
+                color: Colors.white,
+              )),
         ),
         body: Container(
           padding: EdgeInsets.all(20.0),
           child: ListView(children: [
+            IconButton(
+              padding: EdgeInsets.all(0.0),
+              icon: Icon(Icons.production_quantity_limits),
+              iconSize: 40,
+              color: Colors.blue,
+              alignment: Alignment.topLeft,
+              onPressed: () {
+                print("Icon Button clicked");
+              },
+            ),
+            // Icon(
+            //   Icons.production_quantity_limits,
+            //   size: 50,
+            //   color: Colors.purple,
+            // ),
+            Text(
+              "Products",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
             const Text("Add product details in the form below"),
             const SizedBox(height: 10.0),
             MyTextField(
@@ -55,6 +89,7 @@ class _MyFormState extends State<MainForm> {
 
             // checkBox ListTile
             CheckboxListTile(
+              tileColor: Colors.deepPurple.shade50,
               title: Text("Top Product"),
               value: _listTileCheckBox,
               onChanged: (val) {
@@ -64,7 +99,101 @@ class _MyFormState extends State<MainForm> {
               },
               controlAffinity: ListTileControlAffinity.leading,
             ),
+            const SizedBox(height: 5.0),
+            // Radio<ProductTypeEnum>(
+            //   value: ProductTypeEnum.Deliverable,
+            //   groupValue: _productTypeEnum,
+            //   onChanged: (val) {
+            //     setState(() {
+            //       _productTypeEnum = val;
+            //     });
+            //   },
+            // ),
+            Row(
+              children: [
+                Expanded(
+                  child: RadioListTile<ProductTypeEnum>(
+                    contentPadding: EdgeInsets.all(0.0),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(7.0)),
+                    value: ProductTypeEnum.Deliverable,
+                    //dense: true,
+                    groupValue: _productTypeEnum,
+                    tileColor: Colors.deepPurple.shade50,
+                    title: Text(ProductTypeEnum.Deliverable.name),
+                    onChanged: (val) {
+                      setState(() {
+                        _productTypeEnum = val;
+                      });
+                    },
+                  ),
+                ),
+                SizedBox(
+                  width: 5.0,
+                ),
+                Expanded(
+                  child: RadioListTile<ProductTypeEnum>(
+                    contentPadding: EdgeInsets.all(0.0),
+                    tileColor: Colors.deepPurple.shade50,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(7.0)),
+                    //dense: true,
+                    value: ProductTypeEnum.Downloadable,
+                    groupValue: _productTypeEnum,
+                    title: Text(ProductTypeEnum.Downloadable.name),
+                    onChanged: (val) {
+                      setState(() {
+                        _productTypeEnum = val;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10.0),
 
+            // DropDownButton
+            // DropdownButton(
+            //     value: _selectedVal,
+            //     items: _productSizesList
+            //         .map((e) => DropdownMenuItem(
+            //               child: Text(e),
+            //               value: e,
+            //             ))
+            //         .toList(),
+            //     onChanged: (val) {
+            //       setState(() {
+            //         _selectedVal = val as String;
+            //       });
+            //     }),
+
+            DropdownButtonFormField(
+              value: _selectedVal,
+              items: _productSizesList
+                  .map((e) => DropdownMenuItem(
+                        child: Text(e),
+                        value: e,
+                      ))
+                  .toList(),
+              onChanged: (val) {
+                setState(() {
+                  _selectedVal = val as String;
+                });
+              },
+              icon: const Icon(
+                Icons.arrow_drop_down_circle,
+                color: Colors.deepPurple,
+              ),
+              dropdownColor: Colors.deepPurple.shade50,
+              decoration: InputDecoration(
+                labelText: "Product Sizes",
+                prefixIcon: Icon(
+                  Icons.accessibility_new_rounded,
+                  color: Colors.deepPurple,
+                ),
+                border: UnderlineInputBorder(),
+              ),
+            ),
             // Submit Button
             const SizedBox(height: 10.0),
             myBtn(context),
